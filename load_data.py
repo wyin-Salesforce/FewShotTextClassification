1939f80e39fe934ff12f9d47ac9858134e7b0f6f
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-06-17 22:11:17 -0700

    coding

--- load_data.py
+++ load_data.py
@@ -5,9 +5,8 @@ def load_CLINC150():
     readfile = codecs.open('/export/home/Dataset/CLINC150/data_full.json', 'r', 'utf-8')
 
     file2dict =  json.load(readfile)
-    list_of_list = file2dict.get('oos_val')
-    size = len(list_of_list)
-    print(size)
+    for key, value in file2dict.items():
+        print(key, len(value))
 
 if __name__ == "__main__":
     load_CLINC150()
