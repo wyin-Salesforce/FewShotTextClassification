b61792e6830163b94c6481d86077497867953d31
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-06-18 08:18:13 -0700

    coding

--- load_data.py
+++ load_data.py
@@ -36,9 +36,10 @@ domain2intents={'banking':['transfer','transactions','balance','freeze account',
 'sync device']}
 
 def load_CLINC150():
-    gold_intent_set = set()
+    gold_intent_set = []
     for domain, intent_list in domain2intents.items():
-        gold_intent_set+=set(intent_list)
+        gold_intent_set+=intent_list
+    gold_intent_set = set(gold_intent_set)
     readfile = codecs.open('/export/home/Dataset/CLINC150/data_full.json', 'r', 'utf-8')
 
     file2dict =  json.load(readfile)
