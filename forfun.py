a5403f2b11d5af763f87ab268a744af9b5ef14a0
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-06-24 13:15:01 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -2,7 +2,7 @@ import statistics
 
 #529.75
 # initializing list
-test_list = [80.44, 88.44, 85.33, 82, 86.88, 85.77]
+test_list = [88, 79.55, 84, 86.88, 84.88, 86.66]
 print('sum:', sum(test_list))
 average = sum(test_list)/len(test_list)
 res = statistics.pstdev(test_list)
